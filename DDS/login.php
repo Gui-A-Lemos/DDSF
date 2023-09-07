57b9f050e17d4ecfe9092344ff176a14e47a7cee
Guilherme <guilherme.andrade.lemos@gmail.com>
2023-09-07 12:08:11 -0300

    Add files via upload

---
+++ DDS/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+  <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+
+<h1 class="titulo">Bem Vindo!</h1>
+
+  <form class="formulario">
+    <h2 class="subtitulo">Login</h2>
+
+    <label class='Formt' for="email">Email</label>
+    <input class='Formi' placeholder="Insira seu Email" type="email" id="email" name="email">
+
+    <label class='Formt' for="password">Senha</label>
+    <input class='Formi' placeholder="Insira sua Senha"type="password" id="password" name="password">
+
+    <button class="btnlogin">Login</button>
+
+</form>
+
+  
+</body>
+</html>
